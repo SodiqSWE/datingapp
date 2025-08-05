900c23b2f62d1f955cbbd09a66696450e11ccab7
SodiqSWE <sodiq260@gmail.com>
2025-08-05 11:31:43 -0400

    wip

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  private http = inject(HttpClient);
+  protected title = 'Dating app';
+  protected members = signal<any>([])
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: response => this.members.set(response),
+      error: error => console.log(error),
+      complete: () => console.log('Completed the http request'),
+    });
+  }
+
+  // ANOTHER WAY TO DO THE ABOVE
+  // async ngOnInit() {
+  //   this.members.set(await this.getMembers())
+  // }
+
+  // async getMembers() {
+  //   try {
+  //     return lastValueFrom(this.http.get('https://localhost:5001/api/members'))
+  //   } catch (error) {
+  //     console.log(error);
+  //     throw error;
+  //   }
+  // }
+
 }
